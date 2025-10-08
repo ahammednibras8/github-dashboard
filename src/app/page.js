d47a468393d3f14c8c7b26ee9ee5621bca2ea744
ahammednibras8 <ahammednibras@proton.me>
2025-10-08 09:40:19 +0530

    Updated View to 2 vertical Columns

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,15 @@ import ContributionChart from "@/components/ContributionChart";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50 dark:bg-gray-900 flex flex-col items-start justify-start px-6 sm:px-8 lg:px-16 py-8 transition-colors duration-300">
-      <Card />
-      <ContributionChart />
+    <main className="min-h-screen bg-gray-50 dark:bg-gray-900 p-6 sm:p-8 lg:p-12 transition-colors duration-300">
+      <div className="flex flex-col lg:flex-row gap-8 w-full max-w-7xl mx-auto">
+        <div className="lg:w-1/3">
+          <Card />
+        </div>
+        <div className="lg:w-2/3">
+          <ContributionChart />
+        </div>
+      </div>
     </main>
   );
 }
